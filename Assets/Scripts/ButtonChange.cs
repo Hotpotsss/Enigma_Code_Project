e5f557e1ae4c2116526cd78128a9c7051d1308ee
Hotpotsss <ryanlovescod@googlemail.com>
2024-02-11 14:39:55 +0100

    Button and Onclick Button Script

---
+++ Assets/Scripts/ButtonChange.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonChange : MonoBehaviour
+{
+    public Button myButton;
+
+    Button btn = myButton.GetComponent<Button>();
+}
